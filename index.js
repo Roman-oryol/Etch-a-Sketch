e24108d62ffaf554070c87a569610b6e1da210dd
Roman Oryol <roman.oryol83@gmail.com>
2025-01-15 15:12:48 +0200

    feat: add function to generate random RGB color values

--- index.js
+++ index.js
@@ -53,3 +53,12 @@ function resizeGrid() {
   canvas.innerHTML = '';
   createPixels(gridSize);
 }
+
+function getRandomColor() {
+  const getRandomValue = () => Math.floor(Math.random() * 256);
+  const r = getRandomValue();
+  const g = getRandomValue();
+  const b = getRandomValue();
+
+  return `rgb(${r}, ${g}, ${b})`;
+}
